3a98bb13db71a62c2395a844c7e2eb9b92fc2a9f
Gonzales <qajgonzales1@tip.edu.ph>
2023-10-16 22:33:23 +0800

    commited

---
+++ ansible_log.rb
@@ -0,0 +1,14 @@
+2023-10-16 20:58:01,496 p=3826 u=gonzales |  PLAY [Install Nagios on Ubuntu and CentOS] *************************************
+2023-10-16 20:58:01,598 p=3826 u=gonzales |  TASK [Gathering Facts gather_subset=!hardware,!facter,!ohai, gather_timeout=10] ***
+2023-10-16 20:58:02,522 p=3826 u=gonzales |  ok: [192.168.56.103]
+2023-10-16 20:58:02,583 p=3826 u=gonzales |  ok: [192.168.56.105]
+2023-10-16 20:58:02,584 p=3826 u=gonzales |  PLAY RECAP *********************************************************************
+2023-10-16 20:58:02,584 p=3826 u=gonzales |  192.168.56.103             : ok=1    changed=0    unreachable=0    failed=0   
+2023-10-16 20:58:02,584 p=3826 u=gonzales |  192.168.56.105             : ok=1    changed=0    unreachable=0    failed=0   
+2023-10-16 21:07:37,353 p=3901 u=gonzales |  PLAY [Install Nagios on Ubuntu and CentOS] *************************************
+2023-10-16 21:07:37,393 p=3901 u=gonzales |  TASK [Gathering Facts gather_subset=!hardware,!facter,!ohai, gather_timeout=10] ***
+2023-10-16 21:07:38,044 p=3901 u=gonzales |  ok: [192.168.56.103]
+2023-10-16 21:07:38,286 p=3901 u=gonzales |  ok: [192.168.56.105]
+2023-10-16 21:07:38,287 p=3901 u=gonzales |  PLAY RECAP *********************************************************************
+2023-10-16 21:07:38,287 p=3901 u=gonzales |  192.168.56.103             : ok=1    changed=0    unreachable=0    failed=0   
+2023-10-16 21:07:38,287 p=3901 u=gonzales |  192.168.56.105             : ok=1    changed=0    unreachable=0    failed=0   
